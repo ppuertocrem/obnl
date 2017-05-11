df6827f416925d9f71d81a923d010c84154af732
Gillian Basso <gillian.basso@hevs.ch>
2017-05-11 18:04:00 +0200

    server now uses scripts args

--- obnl/server.py
+++ obnl/server.py
@@ -1,5 +1,16 @@
+import argparse
 from obnl.impl.server import Scheduler
 
 if __name__ == "__main__":
-    c = Scheduler('localhost', './../data/initobnl.json', './../data/schedule.json')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host")
+    parser.add_argument("config_file")
+    parser.add_argument("schedule_file")
+
+    args = parser.parse_args()
+
+    # c = Scheduler('localhost', './../data/initobnl.json', './../data/schedule.json')
+
+    c = Scheduler(args.host, args.config_file, args.schedule_file)
     c.start()
